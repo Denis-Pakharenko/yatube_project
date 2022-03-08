f4fcd86d950e35610d04bac6f5afd5cac988e46e
Денис Пахаренко <denis-pakharenko@yandex.ru>
2022-03-08 22:47:13 +0300

    Перед ORM

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -4,8 +4,14 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     template = 'posts/index.html'
-    return render(request, template)
+    context = {
+        'text': 'Это главная страница проекта Yatube'
+    }
+    return render(request, template, context)
 
 def group_posts(request):
     template = 'posts/group_list.html'
-    return render(request, template)
+    context = {
+        'text': 'Здесь будет информация о группах проекта Yatube'
+    }
+    return render(request, template, context)
